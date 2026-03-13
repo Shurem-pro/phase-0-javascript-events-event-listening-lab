b360747563d7cad912bf400b9968c8af96fc1591
Shurem-pro <husseinkadir1227@gmail.com>
2026-03-13 14:16:03 +0300

    commit-initial

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
 function addingEventListener() {
+  // Get the button element by its ID
+  const input = document.getElementById('button');
+  
+  // Add a click event listener to the button
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 }
+
+// Call the function to activate the event listener when the page loads
+addingEventListener();
